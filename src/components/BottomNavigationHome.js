a4301e2ffdd0ca5b74c3ebeb65d972cc43e47b5d
Tomas Vazquez <vazquezt2018@gmail.com>
2023-04-29 10:26:56 -0300

    added new icons in bottom tab

--- src/components/BottomNavigationHome.js
+++ src/components/BottomNavigationHome.js
@@ -1,16 +1,26 @@
-import { View, Text } from "react-native";
 import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen/HomeScreen";
 import HistorialScreen from "../screens/HistorialScreen/HistorialScreen";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 const BottomNavigationHome = () => {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="HomeScreen" component={HomeScreen} />
-      <Tab.Screen name="Historial" component={HistorialScreen} />
+      <Tab.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: "home",
+          tabBarLabel: "Feed",
+        }}
+      />
+      <Tab.Screen
+        name="Historial"
+        component={HistorialScreen}
+        options={{ tabBarIcon: "list-status" }}
+      />
     </Tab.Navigator>
   );
 };
--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,11 +1,9 @@
-import { View, Text, TextInput, StyleSheet } from "react-native";
+import { View, Text } from "react-native";
 import React, { useState } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useNavigation } from "@react-navigation/native";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import Logo from "../../components/Logo";
+import { SafeAreaView } from "react-native-safe-area-context";
 import styles from "./styles";
-import AlertaSuccess from "../../components/AlertaSuccess";
 
 const HomeScreen = () => {
   const teams = ["Boca Juniors", "River Plate", "San Lorenzo", "Racing Club"];
@@ -23,8 +21,6 @@ const HomeScreen = () => {
     <SafeAreaView>
       <View style={styles.container}>
         <Text style={styles.title}>Hola de nuevo! {userActual.email}</Text>
-        <AlertaSuccess />
-        {/* <Logo /> */}
       </View>
     </SafeAreaView>
   );
